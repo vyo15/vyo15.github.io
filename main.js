b728b74095ac43e7ecec691412d5925d4d158ec1
Vio Yusup Iskandar <163681344+vyo15@users.noreply.github.com>
2024-03-18 07:53:41 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// toggle & Responsive Nav
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+        navLists.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+navSlide();
